be9f2a2a0b4387a34989f017707eef99c5271646
Darya <katadasha@yandex.ru>
2018-06-24 13:06:40 +0500

    JS изменение текстка при клике на "Развернуть меню" 1.2

--- js/drop_menu.js
+++ js/drop_menu.js
@@ -1,10 +1,10 @@
 var check = 0;
 $('#drop_menu').click(function() {
 	if (check == 0){
-    $(this).val('Свернуть меню');
+    $(this).text('Свернуть меню');
     check = 1;
   }else{
   	check = 0;
-  	$(this).val('Развернуть меню');
+  	$(this).text('Развернуть меню');
   }
 });
